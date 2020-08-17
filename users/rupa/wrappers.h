0ab6c9f46106c97a28daf1eeaf5e7574ae99db88
rupa deadwyler <rdeadwyler@ns1.com>
2020-08-16 21:07:58 -0400

    wrappers cleanup

--- users/rupa/wrappers.h
+++ users/rupa/wrappers.h
@@ -115,22 +115,4 @@ expanded before being used as arguments to the LAYOUT_xxx macro.
 
 #define ____65_ADJUST___________BL_ROW2    _______, BL_TOGG, BL_BRTG, BL_STEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______
 
-/*
-#define LAYOUT_blocker_base( \
-    K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     K0A,     K0B,     K0C,     K0D,     K0E,     K0F, \
-    K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     K1A,     K1B,     K1C,     K1D,     K1E,     K1E, \
-    K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     K2A,     K2B,     K2C,              K2D,     K2E, \
-    K31,              K32,     K33,     K34,     K35,     K36,     K37,     K38,     K39,     K3A,     K3B,     K3C,     K3D,     K3E, \
-    K41,     K42,     K43,                                K44,                       K45,     K46,              K47,     K48,     K49 \
-  ) \
-  LAYOUT_blocker_wrapper( \
-    K01,     K02,     K03,     K04,     K05,     K06,     K07,     K08,     K09,     K0A,     K0B,     K0C,     K0D,     K0E,     K0F, \
-    K11,     K12,     K13,     K14,     K15,     K16,     K17,     K18,     K19,     K1A,     K1B,     K1C,     K1D,     K1E,     K1E, \
-    K21,     K22,     K23,     K24,     K25,     K26,     K27,     K28,     K29,     K2A,     K2B,     K2C,              K2D,     K2E, \
-    K31,              K32,     K33,     K34,     K35,     K36,     K37,     K38,     K39,     K3A,     K3B,     K3C,     K3D,     K3E, \
-    K41,     K42,     K43,                                K44,                       K45,     K46,     KC_CTL,  K47,     K48,     K49 \
-  )
-#define LAYOUT_blocker_base_wrapper(...)  LAYOUT_blocker_base(__VA_ARGS__)
-*/
-
 // clang-format on
